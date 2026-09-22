15844924dbbaf22fa9911cbbe1670bf962c0c65c
Juan071825 <juanmateoalvarez>
2026-09-22 13:59:46 +0200

    fix(ConexionMySQL.java): :bug: Arreglar importación MySQLException.

--- src/main/java/com/clase/persistencia/ConexionMySQL.java
+++ src/main/java/com/clase/persistencia/ConexionMySQL.java
@@ -2,7 +2,7 @@ package com.clase.persistencia;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLDataException;
+import java.sql.SQLException;
 
 public class ConexionMySQL {
     private static final String URL = "jdbc:mysql://localhost:3306/bbdd";
